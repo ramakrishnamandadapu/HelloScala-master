41da4a5d15d5a1ece1ddbc05f8b1075680c1a6a3
Ramakrishna Mandadapu <ramakrishnamandadapu@users.noreply.github.com>
2015-02-11 16:15:26 -0800

    Update HelloController.scala

--- src/main/scala/hello/HelloController.scala
+++ src/main/scala/hello/HelloController.scala
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import java.util.concurrent.atomic.AtomicLong
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.http.HttpMethod
 
 @RestController
 class HelloController {
